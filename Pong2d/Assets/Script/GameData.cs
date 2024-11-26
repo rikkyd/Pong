b5be29c389c0fc3f8e176f0b782f8906a98a6d18
rikkyd <58492806+rikkyd@users.noreply.github.com>
2024-11-26 15:55:00 +0700

    2

--- Pong2d/Assets/Script/GameData.cs
+++ Pong2d/Assets/Script/GameData.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameData : MonoBehaviour
 {
+
+    [Header("Panels")]
+    public GameObject PausePanel;
+    public GameObject GameOverPanel;
+
+    [Header("InGame UI")]
+    public TextMeshProUGUI timerTxt;
+    public TextMeshProUGUI player1ScoreTxt;
+    public TextMeshProUGUI player2ScoreTxt;
+    public GameObject goldenGoalUI;
+
+    [Header("Game Over UI")]
+    public GameObject player1WinUI;
+    public GameObject player2WinUI;
+    public GameObject youWin;
+    public GameObject youLose;
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+        GameOverPanel.SetActive(false);
         
+        player2WinUI.SetActive(false);
+        player1WinUI.SetActive(false);
+        youWin.SetActive(false);
+        youLose.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+    }
+    public void BackToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("1. Main Menu");
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("1. Gameplay");
     }
 }
--- Pong2d/Assets/Script/MainMenuManager.cs
+++ Pong2d/Assets/Script/MainMenuManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    [Header("Main Menu Panel List")]
+    public GameObject MainPanel;
+    public GameObject HTPPanel;
+    public GameObject TimerPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainPanel.SetActive(true);
+        HTPPanel.SetActive(false);
+        TimerPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+     public void SinglePlayerButton()
+    {
+        TimerPanel.SetActive(true);
+    }
+
+    public void MultiPlayerButton()
+    {
+        TimerPanel.SetActive(true);
+    }
+    
+    public void BackButton()
+    {
+        TimerPanel.SetActive(false);
+        HTPPanel.SetActive(false);
+    }
+
+    public void SetTimerButton(float Timer)
+    {
+        TimerPanel.SetActive(false);
+        HTPPanel.SetActive(true);
+    }
+
+    public void StartBtn()
+    {
+        SceneManager.LoadScene("2. Gameplay");
+    }
+
+    public void ExitGame()
+    {
+       Application.Quit();
     }
 }
